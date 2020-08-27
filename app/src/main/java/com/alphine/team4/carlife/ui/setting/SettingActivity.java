0e10e7e9a5328b2e4ce43c65fb0cb67d9884e02a
Sun-plus-7987 <798746004@qq.com>
2020-08-27 23:15:20 +0800

    添加设置界面的侧边导航栏，但是有一定的bug 注：修改了会直接显示settingactivity设置主界面，如若要回原本界面，请修改配置文件

--- app/src/main/java/com/alphine/team4/carlife/ui/setting/SettingActivity.java
+++ app/src/main/java/com/alphine/team4/carlife/ui/setting/SettingActivity.java
@@ -1,16 +1,39 @@
 package com.alphine.team4.carlife.ui.setting;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
-
 import com.alphine.team4.carlife.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class SettingActivity extends AppCompatActivity {
 
+    private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        navController = Navigation.findNavController(this, R.id.setting_fragment);
+        DrawerLayout drawerLayout = findViewById(R.id.DrawerLayout);
+        appBarConfiguration = new AppBarConfiguration
+//                .Builder(navController.getGraph())
+                .Builder(R.layout.fragment_user,R.id.wifiFragment, R.id.bluethoothFragment)
+                .setDrawerLayout(drawerLayout)
+                .build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationView navigationView = findViewById(R.id.NavigationView);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
