f78ea95974b7fca8f0c1f2d47195ac8b1ff0c77a
jiahao666 <1056780125@qq.com>
2020-09-01 14:01:28 +0800

    发现新闻模块完善3

--- app/src/main/java/com/alphine/team4/carlife/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/alphine/team4/carlife/ui/notifications/NotificationsFragment.java
@@ -1,37 +1,30 @@
 package com.alphine.team4.carlife.ui.notifications;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.TextView;
+
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-
 import com.alphine.team4.carlife.R;
-import com.google.android.material.navigation.NavigationView;
+import com.youth.banner.Banner;
+import com.youth.banner.listener.OnBannerListener;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.Objects;
-import java.util.Timer;
-import java.util.TimerTask;
 
 
 public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
-    private int dt = 4;
-    private TextView tv_dt;
-    private Runnable runnable;
-    private Timer timer;
+
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -41,65 +34,47 @@ public class NotificationsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
 
 
+
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+
+            }
+        });
+
+
         return root;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        tv_dt = (TextView) getActivity().findViewById(R.id.tv_dt);
-        tv_dt.setOnClickListener(new View.OnClickListener() {
+        List images = new ArrayList();
+        images.add(R.drawable.a1);
+        images.add(R.drawable.a2);
+        images.add(R.drawable.a4);
+        images.add(R.drawable.a5);
+        images.add(R.drawable.a6);
+
+
+        Banner banner = (Banner) getActivity().findViewById(R.id.banner);
+        //设置图片加载器
+        banner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+        banner.setImages(images);
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
+
+        //增加点击事件
+        banner.setOnBannerListener(new OnBannerListener() {
             @Override
-            public void onClick(View view) {
-                timer.cancel();
-                startMainActivity();
+            public void OnBannerClick(int position) {
+                Intent i = new Intent(getActivity(),MainActivity.class);
+                startActivity(i);
+                //Toast.makeText(getActivity(), "position"+position, Toast.LENGTH_SHORT).show();
             }
         });
-        countDown();
-
-
-    }
-    @SuppressLint("HandlerLeak")
-    private Handler handler = new Handler(){
-        @SuppressLint("SetTextI18n")
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what){
-                case 1:
-                    tv_dt.setText("点击跳过 " + dt+"s");
-                    dt--;
-                    if(dt<0){
-                        //关闭定时器
-                        timer.cancel();
-                        startMainActivity();
-                    }
-            }
-        }
-    };
-
-    private void countDown(){
-        timer = new Timer();
-        TimerTask timerTask = new TimerTask() {
-            @Override
-            public void run() {
-                handler.sendEmptyMessage(1);
-            }
-        };
-        //开启计时器，时间间隔为1000ms
-        timer.schedule(timerTask,1,1000);
     }
 
-
-    private void startMainActivity() {
-        startActivity(new
-                Intent(getActivity(),MainActivity.class));
-        //关闭当前页面
-        //requireActivity().finish();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        handler.removeCallbacksAndMessages(null);
-    }
 }
